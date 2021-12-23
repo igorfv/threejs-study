53b5c91c8e1b1a0dafffc6c18c08fbc31d551233
Igor Vieira <igorfv@outlook.com>
2021-12-23 15:27:21 -0300

    End of lesson 5

--- lesson-05/main.js
+++ lesson-05/main.js
@@ -3,19 +3,13 @@ import './style.css'
 
 const scene = new THREE.Scene()
 
-const cube = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-
-const mesh = new THREE.Mesh(cube, material)
-scene.add(mesh)
-
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 }
 
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 1000)
-camera.position.set(0, 0, 5)
+camera.position.set(0, 0, 10)
 scene.add(camera)
 
 const canvas = document.querySelector('#webgl')
@@ -23,20 +17,46 @@ const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(sizes.width, sizes.height)
 
 
-// Transform
-mesh.scale.set(2, 0.5, 0.5)
+// Objects
+const group = new THREE.Group()
+scene.add(group)
 
-mesh.rotation.reorder('YXZ')
-mesh.rotation.set(Math.PI * 0.25, Math.PI * 0.25, 0)
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+)
 
-mesh.position.set(0.7, -1, 1)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+)
+
+cube2.position.x = -2
+
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff }),
+)
+
+cube3.position.x = 2
+
+group.add(cube3)
+
+// Transformation
+group.position.set(1, 2, -1)
+group.rotation.y = Math.PI / 4
+group.scale.y = 2
 
 // Axes Helper
 const helper = new THREE.AxesHelper(0.5)
 scene.add(helper)
 
 // Look at
-camera.lookAt(mesh.position)
+camera.lookAt(group.position)
 
 
 renderer.render(scene, camera)
