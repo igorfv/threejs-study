23644e86f7f37880e0865b4efbd8f0f5da630ff7
Igor Vieira <igorfv@outlook.com>
2021-12-23 13:37:43 -0300

    End of lesson 3

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Those are the lessons from [threejs journey](https://threejs-journey.com/)
 ## Lessons
 - [Introduction](./lesson-01)
 - [What is webgl?](./lesson-02)
+- [Basic scene?](./lesson-03)
--- lesson-03/main.js
+++ lesson-03/main.js
@@ -1,6 +1,25 @@
+import * as THREE from 'three'
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene()
+
+const cube = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+
+const mesh = new THREE.Mesh(cube, material)
+scene.add(mesh)
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 1000)
+camera.position.set(0, 0, 5)
+scene.add(camera)
+
+const canvas = document.querySelector('#webgl')
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
